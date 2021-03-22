344ead6e8721307a2b10251039d2ffadb2cf82ba
Yashwanth Muddana <muddana.yashwanth@gmail.com>
2021-03-22 23:47:25 +0530

    app updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import {BrowserRouter as Router,Switch,Route,Redirect} from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import PageNotFound from "./components/404";
-import Home from "./pages/home";
+import Contact from "./pages/contact/contact";
+import Home from "./pages/home/home";
+import Services from "./pages/services/services";
 function App() {
   return (
     <Router>
@@ -9,11 +12,14 @@ function App() {
         <Route exact path="/">
           <Home/>
         </Route>
-        <Route exact path="/about">
+        <Route path="/home">
           <Home/>
         </Route>
         <Route exact path="/contactus">
-          <Home/>
+          <Contact/>
+        </Route>
+        <Route exact path="/services">
+          <Services/>
         </Route>
         <Route exact path="/404">
           <PageNotFound/>
